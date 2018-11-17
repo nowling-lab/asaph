10c06478e1ff81f90a3589548498c7984e3589cd
RJ Nowling <rnowling@gmail.com>
2018-11-17 16:06:52 -0600

    Add t-test

--- asaph/asaph/ml.py
+++ asaph/asaph/ml.py
@@ -21,6 +21,7 @@ import numpy as np
 import numpy.linalg as la
 
 from scipy.stats import chi2
+from scipy.stats import ttest_1samp
 
 from sklearn.decomposition import TruncatedSVD
 from sklearn.ensemble import BaggingClassifier
@@ -78,23 +79,15 @@ def lin_reg_log_likelihood(lr, X, y):
     
     return log_likelihood
 
-def lin_reg_lrtest(X, y, n_iter, set_intercept, g_scaling_factor=1.0):
+def lin_reg_lrtest(X, y, n_iter, g_scaling_factor=1.0):
     null_lr = SGDRegressor(fit_intercept = True, n_iter=n_iter)
     null_X = np.zeros((X.shape[0], 1))
     null_lr.fit(null_X,
                 y)
 
     alt_lr = SGDRegressor(fit_intercept = False, n_iter=n_iter)
-
-    intercept_init = None
-    dof = X.shape[1] - 1
-    if set_intercept:
-        intercept_init = null_lr.intercept_
-        dof = X.shape[1]
-
     alt_lr.fit(X,
-               y,
-               intercept_init = intercept_init)
+               y)
 
     null_likelihood = lin_reg_log_likelihood(null_lr,
                                              null_X,
@@ -106,46 +99,39 @@ def lin_reg_lrtest(X, y, n_iter, set_intercept, g_scaling_factor=1.0):
 
     G = g_scaling_factor * 2. * (alt_likelihood - null_likelihood)
 
-    p_value = chi2.sf(G, dof)
+    p_value = chi2.sf(G, X.shape[1])
 
     p_value = max(1e-300, p_value)
 
     return p_value, alt_lr
 
+def genotype_ttest(X, y):
+    flattened = X.argmax(axis=1)
+
+    # default to 1.0
+    p_values = np.ones(3)
+    for i in range(3):
+        in_group = y[flattened == i]
+        out_group = y[flattened != i]
+
+        # need at least 2 samples to do a t-test
+        if in_group.shape[0] > 1:
+            _, p_value = ttest_1samp(in_group, 0.0)
+            p_values[i] = p_value
+            
+    return p_values
+
 def snp_linreg_pvalues(X, y, g_scaling_factor=1.0):
     N_GENOTYPES = 3
-    gt_p_values = np.zeros(3)
     n_iter = estimate_lr_iter(len(y))
 
-    # when lig reg is performed with all
-    # genotypes as the features, every
-    # data point will have one of the
-    # features "on" so the intercept is not
-    # needed.  If used, we have an extra
-    # variable and then calculated p-value
-    # is larger than it needs to be.
     snp_p_value, model = lin_reg_lrtest(X,
                                         y,
                                         n_iter,
-                                        False,
                                         g_scaling_factor=g_scaling_factor)
 
     gt_pred_ys = model.predict(np.eye(N_GENOTYPES))
-
-    # when lig reg is performed with a single
-    # genotypes as the feature, only some of
-    # the data points will have one of the
-    # features "on" so the intercept is needed
-    # for the others.  If not used, we get a
-    # difference of 0 dof between the null
-    # and alternative models, which is invalid
-    for i in xrange(N_GENOTYPES):
-        p_value, _ = lin_reg_lrtest(X[:, i].reshape(-1, 1),
-                                    y,
-                                    n_iter,
-                                    True,
-                                    g_scaling_factor=g_scaling_factor)
-        gt_p_values[i] = p_value
+    gt_p_values = genotype_ttest(X, y)
 
     return snp_p_value, gt_p_values, gt_pred_ys
 
