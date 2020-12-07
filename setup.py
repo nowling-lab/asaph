1454f8e3fb9b284943e07529ee9e75f12ddca997
RJ Nowling <rnowling@gmail.com>
2020-12-06 20:16:39 -0600

    Remove umap

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(name="asaph",
       license="Apache License, Version 2.0",
       zip_safe=False,
       packages=["asaph"],
-      install_requires = ["numpy", "scipy", "matplotlib", "seaborn", "sklearn", "joblib", "umap-learn"],
+      install_requires = ["numpy", "scipy", "matplotlib", "seaborn", "sklearn", "joblib"],
       scripts=["bin/asaph_import", "bin/asaph_dim_reduc", "bin/asaph_association_tests", "bin/asaph_query"])
 
